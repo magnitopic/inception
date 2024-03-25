6dd90b60e074f5f1ebbb58fe09b06c51f54d4e43
magnitopic <magnitopic@gmail.com>
2024-03-25 14:34:37 +0100

    using wordpress cli

--- srcs/requirements/wordpress/script/start.sh
+++ srcs/requirements/wordpress/script/start.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
-# use Wordpress CLI to configure wordpress
+# install Wordpress CLI
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 
+chmod +x wp-cli.phar
 
+mv wp-cli.phar /usr/local/bin/wp
+
+# use Wordpress CLI to install and configure Wordpress
+wp core download --allow-root
+
+wp config create --dbname=$DATABASE_NAME -dbuser=$WP_USER --dbpass=$DATABASE_PASS
+
+wp core install --url=alaparic.42.fr --title="alaparic" --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASS --admin_email=$WP_APDMIN_EMAIL
+
+php-fpm
