44ab1d743a74f45a971a9cdf2667db4d47e10308
magnitopic <magnitopic@gmail.com>
2024-03-28 20:21:42 +0100

    new way of installing and using wordpress

--- srcs/requirements/mariadb/script/config.sh
+++ srcs/requirements/mariadb/script/config.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-# Create a database and user
-mysql -u root -e "CREATE DATABASE IF NOT EXISTS wp_general"
-mysql -u root -e "GRANT ALL PRIVILEGES ON mydb.* TO '$MARIADB_USERNAME'@'localhost' IDENTIFIED BY '$MARIADB_ROOT_PASSWORD'"
-mysql -u root -e "FLUSH PRIVILEGES"
-
 mkdir -p /run/mysqld
 chown -R mysql:mysql /run/mysqld
+chmod 777 /run/mysqld
+
+service mariadb start
+
+# Create a database and user
+echo "start"
+mysql -u root -e "CREATE DATABASE IF NOT EXISTS $DATABASE_NAME"
+mysql -u root -e "GRANT ALL PRIVILEGES ON $DATABASE_NAME.* TO '$MARIADB_USERNAME'@'%' IDENTIFIED BY '$MARIADB_ROOT_PASSWORD'"
+mysql -u root -e "FLUSH PRIVILEGES"
+echo "end"
+
+service mariadb stop
 
 exec mysqld
--- srcs/requirements/wordpress/script/start.sh
+++ srcs/requirements/wordpress/script/start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Create the user for php-fpm
+useradd -r -s /bin/false $WP_ADMIN_USER
+
 # install Wordpress CLI
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 
@@ -10,9 +13,11 @@ mv wp-cli.phar /usr/local/bin/wp
 # use Wordpress CLI to install and configure Wordpress
 wp core download --allow-root
 
-wp config create --dbname=$DATABASE_NAME -dbuser=$WP_USER --dbpass=$DATABASE_PASS
+# create the wp-config.php file with the database information
+wp config create --allow-root --dbname=$DATABASE_NAME --dbuser=$MARIADB_USERNAME --dbpass=$MARIADB_ROOT_PASSWORD --dbhost=mariadb
 
-wp core install --url=alaparic.42.fr --title="alaparic" --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASS --admin_email=$WP_APDMIN_EMAIL
+# install Wordpress
+wp core install --allow-root --url=alaparic.42.fr --title="alaparic" --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASS --admin_email=$WP_APDMIN_EMAIL
 
 # start php-fpm
-php-fpm7
+/usr/sbin/php-fpm7.4 -F
