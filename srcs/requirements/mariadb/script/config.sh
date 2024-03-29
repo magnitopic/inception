9b379cee1d903ba3ae9f4e8cb85f60af32d22a7c
magnitopic <magnitopic@gmail.com>
2024-03-29 22:21:21 +0100

    trying to connect wordpress to mariadb

--- srcs/requirements/mariadb/script/config.sh
+++ srcs/requirements/mariadb/script/config.sh
@@ -10,12 +10,13 @@ mysql_install_db >/dev/null 2>&1
 echo "-- start of database config file --"
 echo "USE $DATABASE_NAME;" >> /init.sql
 echo "ALETER USER 'root'@'%' INDENTIFIED BY '$DB_ROOT_PASS';" >> /init.sql
-echo "CREATE USER IF NOT EXISTS '$DB_USER' IDENTIFIED BY '$DB_USER_PASS';" >> /init
 echo "CREATE USER IF NOT EXISTS '$DB_ADMIN' IDENTIFIED BY '$DB_ADMIN_PASS';" >> /init.sql
+echo "CREATE USER IF NOT EXISTS '$DB_USER' IDENTIFIED BY '$DB_USER_PASS';" >> /init
 echo "CREATE DATABASE IF NOT EXISTS $DATABASE_NAME;" >> /init.sql
-echo "GRANT ALL PRIVILEGES ON *.* TO '$DB_ADMIN'@'%' WITH GRANT OPTION;" >> /init.sql
+echo "GRANT ALL PRIVILEGES ON *.* TO '$DB_ADMIN'@'%';" >> /init.sql
 echo "GRANT SELECT, INSERT, UPDATE, DELETE ON $DATABASE_NAME.* TO '$DB_USER'@'%';" >> /init.sql
 echo "FLUSH PRIVILEGES;" >> /init.sql
 echo "-- end of database config file --"
 
+echo "-- Starting database --"
 exec mysqld
--- srcs/requirements/wordpress/script/start.sh
+++ srcs/requirements/wordpress/script/start.sh
@@ -1,23 +1,33 @@
 #!/bin/bash
 
+echo "$DB_ADMIN"
+
 # Create the user for php-fpm
 useradd -r -s /bin/false $WP_ADMIN_USER
 
 # install Wordpress CLI
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-
 chmod +x wp-cli.phar
-
 mv wp-cli.phar /usr/local/bin/wp
 
+# check contetion to database
+while ! timeout 1 bash -c 'cat < /dev/null > /dev/tcp/mariadb/3306'; do
+    echo "Waiting for MariaDB to be up and running... 🕐"
+    sleep 1
+done
+echo "Successfully connected to MariaDB ✅"
+
 # use Wordpress CLI to install and configure Wordpress
-wp core download --allow-root
+wp core download --allow-root --path=/wordpress
 
 # create the wp-config.php file with the database information
-wp config create --allow-root --dbname=$DATABASE_NAME --dbuser=$MARIADB_USERNAME --dbpass=$MARIADB_ROOT_PASSWORD --dbhost=mariadb
+wp config create --allow-root --path=/wordpress --dbname=$DATABASE_NAME --dbuser=$DB_ADMIN --dbpass=$DB_ADMIN_PASS --dbhost=mariadb
 
 # install Wordpress
-wp core install --allow-root --url=alaparic.42.fr --title="alaparic" --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASS --admin_email=$WP_APDMIN_EMAIL
+wp core install --allow-root --path=/wordpress --url=localhost --title=alaparicInception --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASS --admin_email=$WP_APDMIN_EMAIL
+
+# create a new author
+wp user create --allow-root $WP_USER $WP_USER_EMAIL --role=author --user_pass=$WP_USER_PASS --path=/wordpress
 
 # start php-fpm
 /usr/sbin/php-fpm7.4 -F
