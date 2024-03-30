b93351ea940cc0f56a7790202910606dc6f0233c
magnitopic <magnitopic@gmail.com>
2024-03-30 19:42:45 +0100

    typo fixes

--- srcs/requirements/mariadb/script/config.sh
+++ srcs/requirements/mariadb/script/config.sh
@@ -8,15 +8,21 @@ mysql_install_db >/dev/null 2>&1
 
 # Configure MariaDB, create a database and user
 echo "-- start of database config file --"
-echo "USE $DATABASE_NAME;" >> /init.sql
-echo "ALETER USER 'root'@'%' INDENTIFIED BY '$DB_ROOT_PASS';" >> /init.sql
+rm -f /init.sql
+echo "USE mysql;" >> /init.sql
+echo "FLUSH PRIVILEGES;" >> /init.sql
+echo "ALTER USER 'root'@'%' IDENTIFIED BY '$DB_ROOT_PASS';" >> /init.sql
 echo "CREATE USER IF NOT EXISTS '$DB_ADMIN' IDENTIFIED BY '$DB_ADMIN_PASS';" >> /init.sql
-echo "CREATE USER IF NOT EXISTS '$DB_USER' IDENTIFIED BY '$DB_USER_PASS';" >> /init
+echo "CREATE USER IF NOT EXISTS '$DB_USER' IDENTIFIED BY '$DB_USER_PASS';" >> /init.sql
 echo "CREATE DATABASE IF NOT EXISTS $DATABASE_NAME;" >> /init.sql
 echo "GRANT ALL PRIVILEGES ON *.* TO '$DB_ADMIN'@'%';" >> /init.sql
-echo "GRANT SELECT, INSERT, UPDATE, DELETE ON $DATABASE_NAME.* TO '$DB_USER'@'%';" >> /init.sql
+echo "GRANT SELECT, INSERT, UPDATE, DELETE ON $DATABASE_NAME.* TO '$DB_ADMIN_PASS'@'%';" >> /init.sql
 echo "FLUSH PRIVILEGES;" >> /init.sql
 echo "-- end of database config file --"
 
+echo "-- Database config file --"
+cat /init.sql
+echo "-- End of database config file --"
+
 echo "-- Starting database --"
-exec mysqld
+mysqld_safe --init-file=/init.sql
