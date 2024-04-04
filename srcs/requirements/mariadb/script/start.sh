19f5f9205cb9ee60b8b31c911993cbf86276af19
magnitopic <magnitopic@gmail.com>
2024-04-04 19:01:36 +0200

    trying to fix connections all around

---
+++ srcs/requirements/mariadb/script/start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "CREATE DATABASE $MYSQL_DATABASE;" >> /etc/mysql/init.sql
+echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> /etc/mysql/init.sql
+echo "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'%' WITH GRANT OPTION;" >> /etc/mysql/init.sql
+echo "FLUSH PRIVILEGES;" >> /etc/mysql/init.sql
+
+mysqld
--- srcs/requirements/wordpress/script/start.sh
+++ srcs/requirements/wordpress/script/start.sh
@@ -1,27 +1,30 @@
 #!/bin/bash
 
-echo "$DB_ADMIN"
-
-# Create the user for php-fpm
-useradd -r -s /bin/false $WP_ADMIN_USER
-
 # install Wordpress CLI
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
 
-# check contetion to database
-while ! timeout 1 bash -c 'cat < /dev/null > /dev/tcp/mariadb/3306'; do
-    echo "Waiting for MariaDB to be up and running... 🕐"
-    sleep 1
+check_connection() {
+  mysql -u $MYSQL_USER -p$MYSQL_PASSWORD -h $MYSQL_HOST -e "SELECT 1;" 2>/dev/null
+}
+
+# connect to db
+while true; do
+  echo "Trying to connect to MYSQL..."
+  if check_connection >/dev/null 2>&1; then
+    break
+  fi
+  sleep 1
 done
-echo "Successfully connected to MariaDB ✅"
+
+echo "Connection successful."
 
 # use Wordpress CLI to install and configure Wordpress
 wp core download --allow-root --path=/wordpress
 
 # create the wp-config.php file with the database information
-wp config create --allow-root --path=/wordpress --dbname=$DATABASE_NAME --dbuser=$DB_ADMIN --dbpass=$DB_ADMIN_PASS --dbhost=mariadb
+wp config create --allow-root --path=/wordpress --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$MYSQL_HOST
 
 # install Wordpress
 wp core install --allow-root --path=/wordpress --url=localhost --title=alaparicInception --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASS --admin_email=$WP_APDMIN_EMAIL
